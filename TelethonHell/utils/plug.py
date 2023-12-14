3b4d37d3a29652503a7664a2ef758f845edce5e6
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-14 23:53:33 +0530

    Update plug.py

--- TelethonHell/utils/plug.py
+++ TelethonHell/utils/plug.py
@@ -67,7 +67,7 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["TelethonHell.plugins." + shortname] = mod
-        LOGS.info("⚡ Hêllẞø† ⚡ - Successfully Imported " + shortname)
+        LOGS.info("🙈 ᴘʙxʙᴏᴛ 🙈 - Successfully Imported " + shortname)
 
 
 # remove plugins
@@ -91,8 +91,8 @@ def remove_plugin(shortname):
 
 async def plug_channel(client, channel):
     if channel != 0:
-        LOGS.info("⚡ Hêllẞø† ⚡ - PLUGIN CHANNEL DETECTED.")
-        LOGS.info("⚡ Hêllẞø† ⚡ - Starting to load extra plugins.")
+        LOGS.info("👻 ᴘʙxʙᴏᴛ 👻 - PLUGIN CHANNEL DETECTED.")
+        LOGS.info("😈 ᴘʙxʙɪᴛ 😈 - Starting to load extra plugins.")
         plugs = await client.get_messages(channel, None, filter=InputMessagesFilterDocument)
         total = int(plugs.total)
         for plugins in range(total):
