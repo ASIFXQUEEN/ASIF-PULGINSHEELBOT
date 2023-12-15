6304ff11b248e6fb9e82d0e1521008f58c36e750
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-15 15:17:49 +0530

    Update alive.py

--- TelethonHell/plugins/alive.py
+++ TelethonHell/plugins/alive.py
@@ -22,6 +22,9 @@ ALIVE_TEMP = """
 ┣─ <b>» Ping:</b> <i>{ping}</i>
 ╰──────────────
 <b><i>»»» <a href='https://t.me/ll_THE_BAD_BOT_ll'>[𝐓ʜᴇ 𝐏ʙ𝐗ʙᴏᴛ]</a> «««</i></b>
+
+<b><i>»»» <a href='https://t.me/II_BAD_MUNDA_II'>[⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂ ꯭м꯭υ꯭η∂꯭α_꯭آآ⎯꯭ ꯭̽🌸]</a> «««</i></b>
+<b><i>»»» <a href='https://t.me/ll_mxni_ll'>[𐏓 ⃪⃝💸 ꯭𝗖ᴜᴛᴇ᳢᪵•𝗗ᴇᴠɪ𝗟 ⃪آ͢آ🦅⃚⃮⃕⃔⃤ ꯭༎ࠫ⛧‌ٖٖٖٖٖٖٜٖٖٖٖ]</a> «««</i></b>
 """
 
 msg = """{}\n
