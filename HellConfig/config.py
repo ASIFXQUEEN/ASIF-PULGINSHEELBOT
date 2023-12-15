ce1b875e9052887c98ba199f0bd4a5b19f83b34a
Mani <140153681+MXNIHACKER@users.noreply.github.com>
2023-12-15 12:52:01 +0530

    Update config.py

--- HellConfig/config.py
+++ HellConfig/config.py
@@ -18,7 +18,7 @@ class Config(object):
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-    GIT_REPO_NAME = os.environ.get("GIT_REPO", "badmunda98/BadXPlugins)
+    GIT_REPO_NAME = os.environ.get("GIT_REPO", "badmunda98/BadXPlugins")
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     HANDLER = os.environ.get("HANDLER", ".")
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
