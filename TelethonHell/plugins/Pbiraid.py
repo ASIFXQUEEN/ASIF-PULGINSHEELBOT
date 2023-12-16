4bb25c1eee83af8d9b8220ab08d92f653099c4ee
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-17 01:10:46 +0530

    Create Pbiraid.py

---
+++ TelethonHell/plugins/Pbiraid.py
@@ -0,0 +1,140 @@
+from . import *
+
+global x
+
+@hell_cmd(pattern="dpbiraid")
+async def _(event):
+    await eor(event, "stop pbi raid")
+    global x
+    x = False
+
+
+@hell_cmd(pattern="pbiraid")
+async def _(event):
+    global x
+    x = True
+    while x == True:
+        await event.delete()
+        chat = event.chat_id
+        await hellbot.send_message(chat, "ਕੱਚ ਦੀ ਗਲਾਸੀ ਵਿਚ")
+        await hellbot.send_message(chat, "ਬੂਟਾ ਭੰਗ ਦਾ")
+        await hellbot.send_message(chat, "ਇੱਕ ਵਾਰ")
+        await hellbot.send_message(chat, "ਦੇਦੇ ਫੇਰ ਨੀ ਮੰਗਦਾ")
+        await hellbot.send_message(chat, "ਕੱਚ ਦੇ ਗਲਸ ਵਿਚ 🥵👅")
+        await hellbot.send_message(chat, "ਤੋਤਾ ਬੋਲਦਾ 🥵👅")
+        await hellbot.send_message(chat, "ਤੇਰੇ ਵਰਗੇ ਦੀ ਮੈ ਤੁਰੇ ਜਾਂਦੇ 🥵👅")
+        await hellbot.send_message(chat, "ਬੁੰਡ ਖੋਲ ਦਾ 🥵👅")
+        await hellbot.send_message(chat, "ਕੱਚ ਦੀ ਗੱਲਾਂਸੀ ਚ 🥵👅")
+        await hellbot.send_message(chat, "ਪੈ ਗਿਆ ਕੱਮ")
+        await hellbot.send_message(chat, "ਤੇਰੀ 🥵👅")
+        await hellbot.send_message(chat, "ਬੁੰਡ ਮਾਰ 🥵👅")
+        await hellbot.send_message(chat, "ਮਾਰ ਕਰ ਦੁ ਕੱਮ 🥵👅")
+        await hellbot.send_message(chat, "ਵਾਰੀ ਵਰਸੀ ਖੱਟਣ ਗਿਆ ਸੀ 🥵👅")
+        await hellbot.send_message(chat, "ਖੱਟ ਕੇ ਲਿਆਂਦਾ ਪੋਲਾ 🥵👅")
+        await hellbot.send_message(chat, "ਤੇਰੀ ਭੈਣ ਦੀ ਫੁੱਦੀ 🥵👅")
+        await hellbot.send_message(chat, "ਤੇਰੀ ਮਾਂ ਦਾ ਪਾਟਿਆ ਸ਼ੋਲਾ 🥵👅")
+        await hellbot.send_message(chat, "ਬਾਰੀ ਬਰਸੀ ਖੱਟਣ ਗਿਆ ਸੀ 🥵")
+        await hellbot.send_message(chat, "ਖੱਟ ਕੇ ਲਿਆਇਆ ਪਿੱਲਾ 🥵👅")
+        await hellbot.send_message(chat, "ਤੇਰੀ ਭੈਣ ਦਾ ਸ਼ੌਲ਼ਾ ਪਾਟ ਗਿਆ 🥵👅")
+        await hellbot.send_message(chat, "ਕਹਿੰਦੀ ਮੈ ਫੁੱਦੀ ਸਹੀ ਕਰਾਉਣੀ 🥵👅")
+        await hellbot.send_message(chat,  "ਬਾਪੂ ਵੇਚ ਇੱਕ ਕਿੱਲਾ 🥵👅")
+        await hellbot.send_message(chat, "🥵ਬਾਰੀ ਬਰਸੀ")
+        await hellbot.send_message(chat, "ਖੱਟਣ ਗਿਆ ਸੀ")
+        await hellbot.send_message(chat, "ਖੱਟ ਕੇ ਲਿਆਂਦਾ")
+        await hellbot.send_message(chat, "ਫੂਸਾ ਮੇਰਾ ਡੈਡੀ")
+        await hellbot.send_message(chat, "ਤੇਰੀ ਬੁੰਡ ਮਾਰੇ ਮੈ")
+        await hellbot.send_message(chat, "ਮਾਰਾਂ ਤੇਰੀ ਭੈਣ ਦਾ ਘੁਸਾ👅") 
+        await hellbot.send_message(chat, "🥵ਬਾਰੀ ਬਰਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟਣ ਗਿਆ ਸੀ 👅") 
+        await hellbot.send_message(chat, "🥵ਖੱਟ ਕੇ ਲਿਆਂਦਾ 👅")
+        await hellbot.send_message(chat, "👅ਸੀਤ ਫੁਲ 🥵👅")
+        await hellbot.send_message(chat, "🥵ਤੇਰੀ ਕੁੜੀ ਦੇ 👅")
+        await hellbot.send_message(chat, "🥵ਸ਼ੋਲ਼ੇ ਚ 👅")
+        await hellbot.send_message(chat, "🥵ਨੀਗਰੋ ਦਾ 👅")
+        await hellbot.send_message(chat,  "🥵55ਇੰਚ ਦਾ 👅")
+        await hellbot.send_message(chat, "🥵ਲੁੱਲ👅")
+        await hellbot.send_message(chat, "🥵ਬਾਰੀ ਬਰਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟਣ ਗਿਆ ਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟ ਕੇ ਲਿਆਇਆ 👅")
+        await hellbot.send_message(chat, "🥵ਕੂਲਾ 👅")
+        await hellbot.send_message(chat,  "🥵ਕੂਲਾ 👅")                                                     
+        await hellbot.send_message(chat, "🥵ਤੇਰੀ ਕੁੜੀ ਦੇ 👅")
+        await hellbot.send_message(chat, "🥵ਲੁੱਲਾ ਮਾਰ ਗਿਆ 👅")
+        await hellbot.send_message(chat, "🥵ਸਾਡੇ ਪਿੰਡ ਵਾਲਾ 👅")
+        await hellbot.send_message(chat, "🥵ਬਾਬਾ ਫੂਲਾ 👅")
+        await hellbot.send_message(chat, "🥵ਬਾਰੀ ਬਰਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟਣ ਗਿਆ ਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟ ਕੇ ਲਿਆਇਆ 👅")
+        await hellbot.send_message(chat, "🥵ਕੇਲਾ 👅")
+        await hellbot.send_message(chat, "🥵ਤੇਰੀ ਭੈਣ 👅")
+        await hellbot.send_message(chat, "🥵ਦਾ ਘੁਸੇ ਚ ਪੈ ਗਿਆ 👅")
+        await hellbot.send_message(chat, "🥵ਤੇਲਾ👅")
+        await hellbot.send_message(chat, "ਕੱਚ ਦੀ ਗਲਾਸੀ ਵਿਚ")
+        await hellbot.send_message(chat, "ਬੂਟਾ ਭੰਗ ਦਾ")
+        await hellbot.send_message(chat, "ਇੱਕ ਵਾਰ")
+        await hellbot.send_message(chat, "ਦੇਦੇ ਫੇਰ ਨੀ ਮੰਗਦਾ")
+        await hellbot.send_message(chat, "ਕੱਚ ਦੇ ਗਲਸ ਵਿਚ 🥵👅")
+        await hellbot.send_message(chat, "ਤੋਤਾ ਬੋਲਦਾ 🥵👅")
+        await hellbot.send_message(chat, "ਤੇਰੇ ਵਰਗੇ ਦੀ ਮੈ ਤੁਰੇ ਜਾਂਦੇ 🥵👅")
+        await hellbot.send_message(chat, "ਬੁੰਡ ਖੋਲ ਦਾ 🥵👅")
+        await hellbot.send_message(chat, "ਕੱਚ ਦੀ ਗੱਲਾਂਸੀ ਚ 🥵👅")
+        await hellbot.send_message(chat, "ਪੈ ਗਿਆ ਕੱਮ")
+        await hellbot.send_message(chat, "ਤੇਰੀ 🥵👅")
+        await hellbot.send_message(chat, "ਬੁੰਡ ਮਾਰ 🥵👅")
+        await hellbot.send_message(chat, "ਮਾਰ ਕਰ ਦੁ ਕੱਮ 🥵👅")
+        await hellbot.send_message(chat, "ਵਾਰੀ ਵਰਸੀ ਖੱਟਣ ਗਿਆ ਸੀ 🥵👅")
+        await hellbot.send_message(chat, "ਖੱਟ ਕੇ ਲਿਆਂਦਾ ਪੋਲਾ 🥵👅")
+        await hellbot.send_message(chat, "ਤੇਰੀ ਭੈਣ ਦੀ ਫੁੱਦੀ 🥵👅")
+        await hellbot.send_message(chat, "ਤੇਰੀ ਮਾਂ ਦਾ ਪਾਟਿਆ ਸ਼ੋਲਾ 🥵👅")
+        await hellbot.send_message(chat, "ਬਾਰੀ ਬਰਸੀ ਖੱਟਣ ਗਿਆ ਸੀ 🥵")
+        await hellbot.send_message(chat, "ਖੱਟ ਕੇ ਲਿਆਇਆ ਪਿੱਲਾ 🥵👅")
+        await hellbot.send_message(chat, "ਤੇਰੀ ਭੈਣ ਦਾ ਸ਼ੌਲ਼ਾ ਪਾਟ ਗਿਆ 🥵👅")
+        await hellbot.send_message(chat, "ਕਹਿੰਦੀ ਮੈ ਫੁੱਦੀ ਸਹੀ ਕਰਾਉਣੀ 🥵👅")
+        await hellbot.send_message(chat,  "ਬਾਪੂ ਵੇਚ ਇੱਕ ਕਿੱਲਾ 🥵👅")
+        await hellbot.send_message(chat, "🥵ਬਾਰੀ ਬਰਸੀ")
+        await hellbot.send_message(chat, "ਖੱਟਣ ਗਿਆ ਸੀ")
+        await hellbot.send_message(chat, "ਖੱਟ ਕੇ ਲਿਆਂਦਾ")
+        await hellbot.send_message(chat, "ਫੂਸਾ ਮੇਰਾ ਡੈਡੀ")
+        await hellbot.send_message(chat, "ਤੇਰੀ ਬੁੰਡ ਮਾਰੇ ਮੈ")
+        await hellbot.send_message(chat, "ਮਾਰਾਂ ਤੇਰੀ ਭੈਣ ਦਾ ਘੁਸਾ👅") 
+        await hellbot.send_message(chat, "🥵ਬਾਰੀ ਬਰਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟਣ ਗਿਆ ਸੀ 👅") 
+        await hellbot.send_message(chat, "🥵ਖੱਟ ਕੇ ਲਿਆਂਦਾ 👅")
+        await hellbot.send_message(chat, "👅ਸੀਤ ਫੁਲ 🥵👅")
+        await hellbot.send_message(chat, "🥵ਤੇਰੀ ਕੁੜੀ ਦੇ 👅")
+        await hellbot.send_message(chat, "🥵ਸ਼ੋਲ਼ੇ ਚ 👅")
+        await hellbot.send_message(chat, "🥵ਨੀਗਰੋ ਦਾ 👅")
+        await hellbot.send_message(chat,  "🥵55ਇੰਚ ਦਾ 👅")
+        await hellbot.send_message(chat, "🥵ਲੁੱਲ👅")
+        await hellbot.send_message(chat, "🥵ਬਾਰੀ ਬਰਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟਣ ਗਿਆ ਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟ ਕੇ ਲਿਆਇਆ 👅")
+        await hellbot.send_message(chat, "🥵ਕੂਲਾ 👅")
+        await hellbot.send_message(chat,  "🥵ਕੂਲਾ 👅")                                                     
+        await hellbot.send_message(chat, "🥵ਤੇਰੀ ਕੁੜੀ ਦੇ 👅")
+        await hellbot.send_message(chat, "🥵ਲੁੱਲਾ ਮਾਰ ਗਿਆ 👅")
+        await hellbot.send_message(chat, "🥵ਸਾਡੇ ਪਿੰਡ ਵਾਲਾ 👅")
+        await hellbot.send_message(chat, "🥵ਬਾਬਾ ਫੂਲਾ 👅")
+        await hellbot.send_message(chat, "🥵ਬਾਰੀ ਬਰਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟਣ ਗਿਆ ਸੀ 👅")
+        await hellbot.send_message(chat, "🥵ਖੱਟ ਕੇ ਲਿਆਇਆ 👅")
+        await hellbot.send_message(chat, "🥵ਕੇਲਾ 👅")
+        await hellbot.send_message(chat, "🥵ਤੇਰੀ ਭੈਣ 👅")
+        await hellbot.send_message(chat, "🥵ਦਾ ਘੁਸੇ ਚ ਪੈ ਗਿਆ 👅")
+        await hellbot.send_message(chat, "🥵ਤੇਲਾ👅")
+        await hellbot.send_message(chat, "😈ਤੇਰੀ ਮਾ ਦਾ ਫੂਦਾ ਮਾਰੇ ਬੈਡ ਹੋ ਕੇ ਟੈਟ😈")
+        await hellbot.send_message(chat, "😈ਬਾਰੀ ਬਰਸੀ ਖਟਣ ਗਿਆ ਸੀ ਖੱਟ ਕੇ ਲਿਆਦਾ ਹੈੱਡ ਤੇਰੀ ਮਾਂ ਦਾ ਫੂਦਾ ਮਾਰੇ ਨਗਾ ਹੋ ਕੇ ਬੈਡ 😈")
+  
+                
+        
+        CmdHelp("pbiraid").add_command(
+    "pbiraid", None, "Big Gaali Spam!"
+).add_command(
+    "dpbiraid", None, " spam stop!"
+).add_warning(
+    "May get floodwait!"
+).add_info(
+    "Spammer Module"
+).add()
+        
+ 
