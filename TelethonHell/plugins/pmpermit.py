0fa0ad986f6699179d18cb991d80666a19a9345c
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-01-19 15:59:32 +0530

    Update pmpermit.py

--- TelethonHell/plugins/pmpermit.py
+++ TelethonHell/plugins/pmpermit.py
@@ -159,7 +159,7 @@ if str(Config.PM_PERMIT).lower() not in disabled_list:
             return
         ForGo10God, _, hell_mention = await client_id(event)
         CSTM_PMP = gvarstat("CUSTOM_PMPERMIT") or None
-        HELL_FIRST = f"🔥 𝙃𝙚𝙡𝙡𝘽𝙤𝙩 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
+        HELL_FIRST = f"🔥 𝗦𝘁𝗿𝗮𝗻𝗴𝗲𝗿 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
         if CSTM_PMP:
              HELL_FIRST += f"\n\n{CSTM_PMP}"
         if event.sender_id == ForGo10God:
@@ -276,7 +276,7 @@ if str(Config.PM_PERMIT).lower() not in disabled_list:
                 return
             ForGo10God, _, hell_mention = await client_id(event)
             CSTM_PMP = gvarstat("CUSTOM_PMPERMIT") or None
-            HELL_FIRST = f"🔥 𝙃𝙚𝙡𝙡𝘽𝙤𝙩 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
+            HELL_FIRST = f"🔥 𝗦𝘁𝗿𝗮𝗻𝗴𝗲𝗿 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
             if CSTM_PMP:
                 HELL_FIRST += f"\n\n{CSTM_PMP}"
             if event.sender_id == ForGo10God:
@@ -393,7 +393,7 @@ if str(Config.PM_PERMIT).lower() not in disabled_list:
                 return
             ForGo10God, _, hell_mention = await client_id(event)
             CSTM_PMP = gvarstat("CUSTOM_PMPERMIT") or None
-            HELL_FIRST = f"🔥 𝙃𝙚𝙡𝙡𝘽𝙤𝙩 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
+            HELL_FIRST = f"🔥 𝗦𝘁𝗿𝗮𝗻𝗴𝗲𝗿 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
             if CSTM_PMP:
                 HELL_FIRST += f"\n\n{CSTM_PMP}"
             if event.sender_id == ForGo10God:
@@ -510,7 +510,7 @@ if str(Config.PM_PERMIT).lower() not in disabled_list:
                 return
             ForGo10God, _, hell_mention = await client_id(event)
             CSTM_PMP = gvarstat("CUSTOM_PMPERMIT") or None
-            HELL_FIRST = f"🔥 𝙃𝙚𝙡𝙡𝘽𝙤𝙩 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
+            HELL_FIRST = f"🔥 𝗦𝘁𝗿𝗮𝗻𝗴𝗲𝗿 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
             if CSTM_PMP:
                 HELL_FIRST += f"\n\n{CSTM_PMP}"
             if event.sender_id == ForGo10God:
@@ -627,7 +627,7 @@ if str(Config.PM_PERMIT).lower() not in disabled_list:
                 return
             ForGo10God, _, hell_mention = await client_id(event)
             CSTM_PMP = gvarstat("CUSTOM_PMPERMIT") or None
-            HELL_FIRST = f"🔥 𝙃𝙚𝙡𝙡𝘽𝙤𝙩 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
+            HELL_FIRST = f"🔥 𝗦𝘁𝗿𝗮𝗻𝗴𝗲𝗿 𝙋𝙈 𝙎𝙚𝙘𝙪𝙧𝙞𝙩𝙮 🔥\n\nHello!! This is an automated message on behalf of {hell_mention}."
             if CSTM_PMP:
                 HELL_FIRST += f"\n\n{CSTM_PMP}"
             if event.sender_id == ForGo10God:
@@ -737,7 +737,7 @@ CmdHelp("pm_permit").add_command(
 ).add_command(
     "unblock", "<in pm>/<reply>", "Unblocks the mentioned user."
 ).add_command(
-    "listapproved", None, "Sends the list of all users approved by Hêllẞø†"
+    "listapproved", None, "Sends the list of all users approved by 𝐒ᴛʀᴀɴɢᴇʀ"
 ).add_info(
     "PM SECURITY"
 ).add_warning(
