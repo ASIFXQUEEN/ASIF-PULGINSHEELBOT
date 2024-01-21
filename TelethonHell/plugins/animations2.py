382f35d092ab12dd58be985d9fd9a51f01acbbc7
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-01-21 15:00:16 +0530

    Update animations2.py

--- TelethonHell/plugins/animations2.py
+++ TelethonHell/plugins/animations2.py
@@ -153,7 +153,7 @@ async def _(event):
         idd = reply_message.sender_id
         if idd == 1432756163:
             await event.edit(
-                "This is My Master\n**How dare you trying to tell me to kill master nigger!**\n\n__Your account is on hold! Pay 99$ to my master__ [HellBoy](https://t.me/Forgo10God) __to release your account__😏"
+                "This is My Master\n**How dare you trying to tell me to kill master nigger!**\n\n__Your account is on hold! Pay 99$ to my master__ [SHIVANSH](https://t.me/SHIVANSH474) __to release your account__😏"
             )
         else:
             await event.edit("`K i l l i n g . . . . . . .`")
